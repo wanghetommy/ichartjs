6b2fbc0e46e8fcf287b34ee4f0ba2419b60b1e27
wanghetommy <wanghetommy@gmail.com>
2013-12-02 14:25:15 +0800

    update gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,26 @@ module.exports = function(grunt){
 
     var banner = '/**\n'+
         '* <%= pkg.name %> Library v<%= pkg.version %> http://www.ichartjs.com/\n'+
-        '* @date <%= grunt.template.today("yyyy-mm-dd") %>\n'+
+        '* @date <%= grunt.template.today("yyyy-mm-dd hh:MM") %>\n'+
         '* @author taylor wong\n'+
         '* @Copyright 2013 wanghetommy@gmail.com Licensed under the Apache License, Version 2.0 (the "License");\n'+
         '* you may not use this file except in compliance with the License.\n'+
         '* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0\n'+
         '*/;\n';
 
-    // 项目配置
+    var srcFile =  'ichart.<%= pkg.version %>.src.js';
+    var minFile =  'ichart.<%= pkg.version %>.min.js';
+
+
+    grunt.file.defaultEncoding = 'utf8'
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
-            options: {
-                //separator: ';',
-                banner:banner
-            },
-            dist: {
+            cat:{
+                options: {
+                },
                 src: [
-                    'src/ichart.core.js',
                     'src/ichart.element.js',
                     'src/ichart.html.js',
                     'src/ichart.painter.js',
@@ -44,6 +46,7 @@ module.exports = function(grunt){
                     'src/ichart.bar.js',
                     'src/ichart.bar2d.js',
                     'src/ichart.barstacked2d.js',
+                    'src/ichart.barmulti2d.js',
                     'src/ichart.tip.js',
                     'src/ichart.text.js',
                     'src/ichart.pie.js',
@@ -58,7 +61,18 @@ module.exports = function(grunt){
                     'src/ichart.linebasic2d.js',
                     'src/ichart.area2d.js'
                 ],
-                dest: 'ichart.<%= pkg.version %>.src.js'
+                dest: srcFile
+            },
+            dog:{
+                options: {
+                    //separator: ';',
+                    banner:banner
+                },
+                src: [
+                    'src/ichart.core.js',
+                    srcFile
+                ],
+                dest: srcFile
             }
         },
         uglify: {
@@ -66,17 +80,53 @@ module.exports = function(grunt){
                 banner:banner
             },
             build: {
-                src: 'ichart.<%= pkg.version %>.src.js',
-                dest: 'ichart.<%= pkg.version %>.min.js'
+                src: srcFile,
+                dest: minFile
             }               
+        },
+        build:{
+            options: {
+                banner:'(function($){\n',
+                footer:'\n})(iChart);',
+                target:srcFile,
+                date:'<%= grunt.template.today("yyyy-mm-dd hh:MM") %>'
+            }
+        },
+        copy:{
+            options: {
+                src:srcFile,
+                min:minFile,
+                destSrc :  'ichart.<%= pkg.version %>.src.beta.<%=grunt.template.today("yyyymmdd")%>.js',
+                destMin:'ichart.<%= pkg.version %>.min.beta.<%=grunt.template.today("yyyymmdd")%>.js',
+                dir:'../build_ichartjs/<%=grunt.template.today("yyyymmdd")%>/'
+            }
         }
-
     });
 
-    // 加载提供"uglify"任务的插件
+    grunt.registerTask('build', 'build code', function() {
+        var options = grunt.config.get(this.name).options;
+        var target = options.target;
+        if(!grunt.file.exists(target)){
+            grunt.fail.warn(target+' is not found.');
+        }
+        var content = grunt.file.read(target);
+        content = options.banner + content.replace(/iChart/g,"$")+options.footer;
+        grunt.file.write(target,content);
+        grunt.log.writeln("File "+target+" build success.");
+    });
+
+    grunt.registerTask('copy', 'copy js to build', function() {
+        var options = grunt.config.get(this.name).options;
+        grunt.file.copy(options.src,options.dir+options.destSrc);
+        grunt.file.copy(options.min,options.dir+options.destMin);
+        grunt.log.writeln("File copy to "+options.dir+" success.");
+    });
+
+
+    //loading plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
-    // 默认任务
-    grunt.registerTask('default', ['concat','uglify']);
+    //default task
+    grunt.registerTask('default', ['concat:cat','build','concat:dog','uglify','copy']);
 }
