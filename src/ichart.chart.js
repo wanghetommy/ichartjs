b8cfc513d3b239b308caa0979ac66cc70c3fedcd
Anderson Mao <andersonmao@gmail.com>
2014-06-10 13:26:25 +0800

    Update ichart.chart.js
    Allow 0 as column value

--- src/ichart.chart.js
+++ src/ichart.chart.js
@@ -87,7 +87,7 @@
 				item = [],T = 0;
 				c.each(function(d,j){
 					V = d.value[i];
-					if(!V)return;
+					if(V==null) return; // Allow V to be 0. Not use if(!V)
 					d.value[i] = V =  pF(V,V);
 					T+=V;
 					if(stack){
